45c0c9345c6d4507b529891087eeb3213758ef3c
Aditi-Mokashi <aditimokashi23@gmail.com>
2024-02-03 14:43:48 +0530

    feat: added log message and try except

--- utils/read_config.py
+++ utils/read_config.py
@@ -1,5 +1,8 @@
 import json
 
+from utils import logger
+
+
 def get_config_data() -> list:
     """
     reads config.json file to get URLs of news articles
@@ -7,6 +10,9 @@ def get_config_data() -> list:
     Returns:
         list: list of URLs of news articles
     """
-    with open("config.json", 'r') as f:
-        data = json.load(f)
-    return data['urls']
+    try:
+        with open("config.json", 'r') as f:
+            data = json.load(f)
+        return data['urls']
+    except IOError as e:
+        logger.log_message(message=f"Error while reading file: {e.args}", level=40)
